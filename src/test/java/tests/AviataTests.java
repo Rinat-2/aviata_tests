05846be21c9b9b097f2ac05833a00ceefddbc87a
Rinat <rinat.astr2@gmail.com>
2020-10-28 14:11:52 +0600

    Added Allure

---
+++ src/test/java/tests/AviataTests.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.clickViaJs;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+
+public class AviataTests extends TestBase {
+
+    @Test
+    void simpleUITest(){
+        Configuration.timeout = 15000;
+        step ("Заходим на главную страницу", () -> {
+            open("https://aviata.kz/");
+        });
+        step("Нажимаем на предлагаемые популярные направления",() -> {
+            $$(".ux-pop-orig").find(visible).sibling(1).click();
+            $$(".ux-pop-dest").find(visible).sibling(0).click();
+        });
+
+        step("Выбираем дату поездки", () -> {
+            $("#desktop-one-way-from-date").click();
+            $(".ui-datepicker-today").click();
+        });
+
+        step("Нажимаем на кнопку найти билеты", () -> {
+            $$(".ux-main-search").find(visible).click();
+        });
+
+        step("Проверяем на правильность введенных городов", () -> {
+            $((byName("from"))).shouldHave(value("Шымкент"));
+            $((byName("to"))).shouldHave(value("Алматы"));
+        });
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,16 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
+
+public class TestBase {
+
+    @BeforeAll
+    public static void beforeAll() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+    }
+}
